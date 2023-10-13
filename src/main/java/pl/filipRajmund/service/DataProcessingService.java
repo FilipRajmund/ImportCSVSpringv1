0f65a6d939311fac4a196949e3728bcb25ccd2ca
FilipRajmund <filip.kopka@poczta.fm>
2023-10-13 15:13:59 +0200

    New Fature [1] sorted list by stream

--- src/main/java/pl/filipRajmund/service/DataProcessingService.java
+++ src/main/java/pl/filipRajmund/service/DataProcessingService.java
@@ -1,14 +1,21 @@
 package pl.filipRajmund.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import pl.filipRajmund.domain.User;
 
+import java.util.Comparator;
 import java.util.List;
-
+@Slf4j
 @AllArgsConstructor
 public class DataProcessingService {
     private final StringPreparationSevice stringPreparationSevice;
-    public List<String> process(final List<User> user){
+    public List<String> process(final List<User> users){
+        return users.stream()
+                .sorted(Comparator.comparing(User::getBirthDate))
+                .map(user->stringPreparationSevice.prepare(user))
+                .peek(line->log.info("Prepared line: [{}]",line))
+                .toList();
 
     }
 }
