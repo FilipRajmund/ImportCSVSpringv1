5fd45f64b2659a61ede509a1b69ba4f5acaf4ed3
FilipRajmund <filip.kopka@poczta.fm>
2023-10-13 16:10:42 +0200

    New Fature [2] read data and write to List<User>

--- src/main/java/pl/filipRajmund/service/DataRepository.java
+++ src/main/java/pl/filipRajmund/service/DataRepository.java
@@ -1,9 +1,17 @@
 package pl.filipRajmund.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
 import pl.filipRajmund.domain.User;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Repository
@@ -11,6 +19,32 @@ public class DataRepository {
 
     //metoda tworzy liste userow z dostarczongo pliku
     public List<User> readCSV() {
+        try {
+            return Files.readAllLines(ResourceUtils.getFile("classpath:users.csv").toPath()).stream()
+                    .map(line -> mapUser(line))
+                    .filter(user-> user.isPresent())
+                    .map(user->user.get())
+                    .toList();
+
+                    //.filter(user->user.isPresent())
+        } catch (IOException ex) {
+            log.error("Error reading users.csv");
+            return List.of();
+        }
+
+    }
+
+    private Optional<User> mapUser(String line) {
+        if (!line.contains("@")) {
+            return Optional.empty();
+        }
+        List<String> lineAsList = Arrays.asList(line.split(";"));
+        return Optional.of(User.builder()
+                .email(lineAsList.get(0))
+                .name(lineAsList.get(1))
+                .publicName(lineAsList.get(2))
+                .birthDate(LocalDate.parse(lineAsList.get(3)))
+                .build());
 
     }
 
