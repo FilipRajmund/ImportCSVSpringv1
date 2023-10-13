74830b1300f951664026a1f7ea5181e7f14e7f68
FilipRajmundDom <filip.kopka@poczta.fm>
2023-10-13 20:00:47 +0200

    New Main [1] Running without Spring Beans

--- src/main/java/pl/filipRajmund/Main.java
+++ src/main/java/pl/filipRajmund/Main.java
@@ -1,7 +1,14 @@
 package pl.filipRajmund;
 
+import pl.filipRajmund.service.DataProcessingService;
+import pl.filipRajmund.service.DataRepository;
+import pl.filipRajmund.service.MainProcessor;
+import pl.filipRajmund.service.StringPreparationSevice;
+
 public class Main {
     public static void main(String[] args) {
+        MainProcessor mainProcessor =new MainProcessor(new DataRepository(),new DataProcessingService(new StringPreparationSevice()));
+        mainProcessor.calculate();
 
     }
 }
